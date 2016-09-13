1beaee1f4f160352645c3a8986711526d7d39d0a
Javier Martinez <javier.martinez@um.es>
2016-09-13 15:16:57 +0200

    last

--- src/test/Main.java
+++ src/test/Main.java
@@ -7,8 +7,9 @@ public class Main {
 		
 			Operaciones op = new Operaciones();
 			//System.out.println(op.esPrimo(10));
-			//op.mostrarDivisores(28);
-			System.out.println(op.mostrarDivisores2(8));
+			//op.mostrarDivisores(8);
+			//System.out.println(op.mostrarDivisores2(8));
+			System.out.println(op.test(8));
 
 	}
 
--- src/test/Operaciones.java
+++ src/test/Operaciones.java
@@ -27,13 +27,13 @@ public class Operaciones {
 			}				
 		}
 	}
-	
+
 	public static int mostrarDivisores2(int numero){
 		if(numero==0){
 			System.out.println("No ceros!");
 			//return numero;
 		}else{
-			for(int i=1;i<numero+1;i++){
+			for(int i=2;i<numero+1;i++){
 				if(numero%i == 0){
 					return i;	
 				}
@@ -43,6 +43,14 @@ public class Operaciones {
 		return numero;
 	}
 
+	public static int test(int numero){
+		
+		for(int i=1;i<numero+1;i++){
+			return i+numero;	
+		}
+		return numero;
+	}
+	
 //	public static void main(String[] args){
 //		Operaciones op = new Operaciones();
 //		System.out.println(op.esPrimo(10));
