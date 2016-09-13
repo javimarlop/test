a94b5c3d19f3dd6c2bfb684ed4327ccaf414d553
Java <Java@192.168.0.53>
2016-09-13 13:28:21 +0200

    last

---
+++ src/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			Operaciones op = new Operaciones();
+			//System.out.println(op.esPrimo(10));
+			//op.mostrarDivisores(28);
+			System.out.println(op.mostrarDivisores2(8));
+
+	}
+
+}
--- src/test/Operaciones.java
+++ src/test/Operaciones.java
@@ -15,5 +15,37 @@ public class Operaciones {
 		}
 		return true;
 	}
+	
+	public static void mostrarDivisores(int numero){
+		if(numero==0){
+			System.out.println("No ceros!");
+		}else{
+			for(int i=1;i<numero+1;i++){
+				if(numero%i == 0){
+					System.out.println(i);	
+				}
+			}				
+		}
+	}
+	
+	public static int mostrarDivisores2(int numero){
+		if(numero==0){
+			System.out.println("No ceros!");
+			//return numero;
+		}else{
+			for(int i=1;i<numero+1;i++){
+				if(numero%i == 0){
+					return i;	
+				}
+			}				
+		}
+		System.out.println("Sin divisores!");
+		return numero;
+	}
 
+//	public static void main(String[] args){
+//		Operaciones op = new Operaciones();
+//		System.out.println(op.esPrimo(10));
+//	}
+	
 }
